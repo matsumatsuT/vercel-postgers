2f60054b3e10a5141441bc1a8decae9dd752b8ab
matsumoto <dsongben@gmail.com>
2024-09-29 13:48:52 +0900

    server actionを使用する

--- src/app/list/ListPage.tsx
+++ src/app/list/ListPage.tsx
@@ -1,33 +1,32 @@
 "use client"
 
+import { getProfile } from "@/app/(actions)/getProfile"
 import { Profile } from "@/app/list/Profile"
 import { User, Profile as typeProfile } from "@/prisma-types"
 import { useState } from "react"
+import { useFormState } from "react-dom"
 type Props = {
   userData: User[]
 }
 
 export const ListPage = ({ userData }: Props) => {
-  const [profile, setProfile] = useState<typeProfile>()
-
-  const onClick = async (userId: number) => {
-    const res = await fetch(`/profile/${userId}`)
-    const data: typeProfile = await res.json()
-    setProfile(data)
-  }
+  const [state, formAction] = useFormState(getProfile, null)
 
   return (
     <div>
       <ul style={{ display: "flex", flexDirection: "column", gap: 5 }}>
         {userData.map((v) => (
           <li key={v.id}>
-            <div>name:{v.name}</div>
-            <div>email:{v.email}</div>
-            <button onClick={() => onClick(v.id)}>プロフィールを取得</button>
+            <form action={formAction}>
+              <div>name:{v.name}</div>
+              <div>email:{v.email}</div>
+              <input type="hidden" name="userId" value={v.id} />
+              <button type="submit">プロフィールを取得</button>
+            </form>
           </li>
         ))}
       </ul>
-      {profile && <Profile profile={profile} />}
+      {state && <Profile profile={state} />}
     </div>
   )
 }
