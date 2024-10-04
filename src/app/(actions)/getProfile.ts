8ae655ea8c24230143532a500bde2f0acad65698
matsumoto <dsongben@gmail.com>
2024-10-04 19:12:12 +0900

    リセット

--- src/app/(actions)/getProfile.ts
+++ src/app/(actions)/getProfile.ts
@@ -1,16 +1,16 @@
-"use server"
+// "use server"
 
-import { prisma } from "@/lib/prisma"
-import { Profile } from "@prisma/client"
+// import { prisma } from "@/lib/prisma"
+// import { Profile } from "@prisma/client"
 
-export async function getProfile(state: Profile | null, formData: FormData) {
-  const userId = formData.get("userId")
+// export async function getProfile(state: Profile | null, formData: FormData) {
+//   const userId = formData.get("userId")
 
-  if (!userId) return null
+//   if (!userId) return null
 
-  const res = await prisma.profile.findUnique({
-    where: { userId: Number(userId) },
-  })
+//   const res = await prisma.profile.findUnique({
+//     where: { userId: Number(userId) },
+//   })
 
-  return res
-}
+//   return res
+// }
