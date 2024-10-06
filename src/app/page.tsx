3cbea1af84593f32ed3a657ff8de809d6f4b8032
matsumoto <dsongben@gmail.com>
2024-10-06 16:14:59 +0900

    一覧取得

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
+import { prisma } from "@/lib/prisma";
 import { CreateForm } from "../components/CreateForm";
 
-export default function Home() {
+export default async function Home() {
+  const data = await prisma.user.findMany();
+
   return (
     <>
       <CreateForm />
-      <div>スタート</div>
+      <hr />
+      <h1>ユーザ一覧</h1>
+      <ul style={{ display: "grid", gap: 5 }}>
+        {data.map((user) => (
+          <li key={user.id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
